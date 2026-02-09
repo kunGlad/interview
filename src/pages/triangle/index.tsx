5fcba04358177952c7ca2ac017e8c57fe757a74e
zhaokun <1613430161@qq.com>
2026-02-09 19:21:36 +0800

    空心三角形

--- src/pages/triangle/index.tsx
+++ src/pages/triangle/index.tsx
@@ -3,8 +3,8 @@ import "./index.css";
 const Triangle = () => {
   return (
     <div>
-      这是一个三角形
       <div className="box"></div>
+      <div className="empty-center-box"></div>
     </div>
   );
 };
